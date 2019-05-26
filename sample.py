bbeb52f9697629ed0ceaa86b959249157da183cb
sa--sondarkar <saisondarkar18@gmail.com>
2019-05-26 13:24:46 +0530

    Final Code

--- sample.py
+++ sample.py
@@ -1,8 +1,12 @@
 from flask import Flask
 #imported Flask in our this Python file
 
+
+
 app = Flask(__name__)
 #Created a constructor of the Flask Object
+
+
 
 #Decorator for the Route creation
 @app.route("/")
@@ -10,6 +14,10 @@ def hello_sample_function():
     #Sample function for the API to host
     return "http://bit.ly/lusmapleapi"
 
+
+
 #If the file running directly please run the APP
 if __name__ == "__main__":
     app.run(debug= True)
+
+
